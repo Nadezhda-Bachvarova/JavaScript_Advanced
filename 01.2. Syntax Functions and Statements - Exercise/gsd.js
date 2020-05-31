516ce9999308b78b1ef4b3034514648d8e958f6f
Nadezhda-Mihaylova <64796417+Nadezhda-Mihaylova@users.noreply.github.com>
2020-05-31 13:43:08 +0300

    Create gsd.js

---
+++ 01.2. Syntax Functions and Statements - Exercise/gsd.js
@@ -0,0 +1,15 @@
+"use strict"
+
+function solve(a, b) {
+    const small = Math.min(a, b);
+    let gcd = 1;
+
+    for (let i = 1; i <= small; i++) {
+        if (a % i == 0 && b % i == 0) {
+            gcd = i;
+        }
+    }
+    console.log(gcd);
+}
+
+solve(2154, 458)
