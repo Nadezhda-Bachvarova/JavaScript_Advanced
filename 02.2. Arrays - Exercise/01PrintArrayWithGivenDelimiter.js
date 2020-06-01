acacc2711b6c9b001b36c4b5c477f18f0dd4c186
Nadezhda-Mihaylova <64796417+Nadezhda-Mihaylova@users.noreply.github.com>
2020-06-02 01:16:02 +0300

    Create 01PrintArrayWithGivenDelimiter.js

---
+++ 02.2. Arrays - Exercise/01PrintArrayWithGivenDelimiter.js
@@ -0,0 +1,15 @@
+
+
+function delimiter(arr) {
+    let del = arr.pop();
+    return arr.join(del);
+}
+
+console.log(delimiter(['One','Two','Three','Four','Five','-']))
+console.log(delimiter(['How about no?',
+'I',
+'will',
+'not',
+'do',
+'it!',
+'_']))
