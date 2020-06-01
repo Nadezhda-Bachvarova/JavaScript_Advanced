6eda66fec2572278c3b1f38439be99858448d7a8
Nadezhda-Mihaylova <64796417+Nadezhda-Mihaylova@users.noreply.github.com>
2020-06-02 01:57:40 +0300

    Create 06SortArray.js

---
+++ 02.2. Arrays - Exercise/06SortArray.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function sortArray(array) {
+    return array.sort((a,b) => a.length - b.length || a.localeCompare(b)).join('\n');
+
+}
+
+console.log(sortArray(['test',
+'Deny',
+'omen',
+'Default']))
