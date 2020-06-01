f7c1bc5ad468989ea190297343faa6c1f37d6104
Nadezhda-Mihaylova <64796417+Nadezhda-Mihaylova@users.noreply.github.com>
2020-06-02 01:57:31 +0300

    Create 04RotateArray.js

---
+++ 02.2. Arrays - Exercise/04RotateArray.js
@@ -0,0 +1,14 @@
+function rotate(arr) {
+    let n = Number(arr.pop());
+    n = n % arr.length;
+    for (let i = 0; i < n; i++) {
+        arr.unshift(arr.pop());
+    }
+    return arr.join(' ');
+}
+
+console.log(rotate(['1',
+'2',
+'3',
+'4',
+'2']))
