65e0a88aa148e2d7e58e5f6b95757553a0981c4e
Nadezhda-Mihaylova <64796417+Nadezhda-Mihaylova@users.noreply.github.com>
2020-06-10 00:20:38 +0300

    Create 02. JSONsTable.js

---
+++ 03.2. Objects and Classes - Exercise/02. JSONsTable.js
@@ -0,0 +1,22 @@
+"use strict"
+
+function solve(input) {
+    const rows = [];
+    //обхождане на входа
+    for (let line of input) {
+        //JSON.parse на елементите
+        const person = JSON.parse(line);
+        //съставвяме низ, съдържащ HTML ред със стойности от обекта
+        rows.push(`\t<tr>\n\t\t<td>${person.name}</td>\n\t\t<td>${person.position}</td>\n\t\t<td>${person.salary}</td>\n\t</tr>`);
+    }
+    //отпечатваме началото на таблицата
+    console.log('<table>')
+    //отпечатваме резултата
+    console.log(rows.join('\n'));
+    //отпечатваме края на таблицата
+    console.log('</table>')
+}
+
+solve(['{"name":"Pesho","position":"Promenliva","salary":100000}',
+'{"name":"Teo","position":"Lecturer","salary":1000}',
+'{"name":"Georgi","position":"Lecturer","salary":1000}'])
